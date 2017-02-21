9fb3451c586f6982135b996fe74da933233c3001
praveen <kalamsuthar@gmail.com>
2017-02-22 01:41:35 +0530

    Third commit

--- Life.java
+++ Life.java
@@ -2,6 +2,7 @@ public class Life{
   public static void main(String [] args){
          System.out.println("Hello Life");
   System.out.println("Fool");
+  System.out.println("ok git");
       }
 
 }
